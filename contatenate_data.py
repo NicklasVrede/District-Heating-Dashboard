bd96a4d531bec021e3ce7addd95ceda5566c9802
Nicklas Vrede <32699906+NicklasVrede@users.noreply.github.com>
2024-09-18 09:54:21 +0200

    d

--- contatenate_data.py
+++ contatenate_data.py
@@ -8,7 +8,10 @@ grouped_data = pd.read_excel('Visualisering/UFM_samlet_06SEP2024_grouped.xlsx')
 
 
 #print top 5 uddannelser med højest frafaldsprocent
-print(grouped_data.nlargest(5, 'afbrud'))
+print(f'frafal: {grouped_data.nlargest(5, 'afbrud')}')
 
 #print top 5 uddannelser med højest skiftende arbejdsform
-print(grouped_data.nlargest(5, 'arbejdsform_skiftende_pct'))
+print(f'arbejdsform_skiftende_pct: {grouped_data.nlargest(5, 'arbejdsform_skiftende_pct')}')
+
+#print top 5 uddannelser med højest ledighed_nyudd
+print(f'ledighed_nyudd: {grouped_data.nlargest(5, 'ledighed_nyudd')}')
