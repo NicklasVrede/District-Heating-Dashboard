811dc7a821ed651d7d1a28f1535e18fdb88edc6d
Nicklas Vrede <32699906+NicklasVrede@users.noreply.github.com>
2024-12-05 09:02:20 +0100

    better fix

--- utils/javascript/focusLayers/DefaultFocus.js
+++ utils/javascript/focusLayers/DefaultFocus.js
@@ -1,4 +1,3 @@
-import { updateGraph } from '../../../graphs/graphManager.js';
 import { selectionSet } from '../../../main.js';
 import { yearState } from './YearState.js';
 import { municipalitiesVisible } from '../municipalitiesFunctions.js';
@@ -10,31 +9,10 @@ export class DefaultFocus {
     }
 
     apply() {
-        console.log('DefaultFocus applying with focus: default');
-        
-        if (this.map) {
-            if (!municipalitiesVisible) {
-                this.map.setLayoutProperty('plants', 'visibility', 'visible');
-            }
-            this.map.setLayoutProperty('plants-price', 'visibility', 'none');
-        }
-        
-        if (this.measureContainer) {
-            this.measureContainer.classList.remove('visible');
-            this.measureContainer.classList.add('hidden');
-        }
-
-        const selectedForsyids = Array.from(selectionSet);
-        yearState.visible = selectedForsyids.length >= 3;
-
-        const data = window.dataDict;
-        if (data && selectedForsyids.length > 0) {
-            console.log('DefaultFocus: Calling createOrUpdatePlotlyGraph with default focus');
-            updateGraph();
-        }
+        // No specific actions needed for default focus
     }
 
     remove() {
-        //unused
+        // Unused, removal is handled elsewhere
     }
-} 
+}
