99d71e731e51d476aee9b24ec120e67558336304
Nicklas Vrede <32699906+NicklasVrede@users.noreply.github.com>
2024-12-19 14:30:50 +0100

    price hotfix

--- utils/javascript/focusLayers/PriceFocus.js
+++ utils/javascript/focusLayers/PriceFocus.js
@@ -121,10 +121,13 @@ export class PriceFocus {
             
             data.features = data.features.map(feature => {
                 const id = municipalitiesVisible ? 
-                    feature.properties.lau_1 : 
-                    feature.properties.forsyid;
+                    feature.properties.lau_1.padStart(8, '0') : 
+                    feature.properties.forsyid.padStart(8, '0');
                 
+                console.log('Looking up price for ID:', id, 'Year:', currentYear);
                 const priceData = window.dataDict?.[id]?.prices?.[currentYear]?.mwh_price ?? null;
+                console.log('Price data found:', priceData);
+                
                 feature.properties.current_price = priceData;
                 feature.properties.price_rank = this.priceRankings?.[id]?.rank || 0;
                 return feature;
@@ -169,15 +172,30 @@ export class PriceFocus {
             this.map.setPaintProperty('plants-price', 'circle-stroke-color', 'white');
         }
         else {
-            this.map.setPaintProperty('municipalities-price', 'fill-color', [
+            console.log('Price range:', priceRange);
+            console.log('Setting municipality colors with condition:', [
                 'case',
                 ['any',
-                    ['==', ['string', ['get', 'forsyids']], ''],  // No plants
+                    ['==', ['string', ['get', 'forsyids']], ''],
                     ['all',
-                        ['!=', ['string', ['get', 'forsyids']], ''],  // Has plants
-                        ['==', ['get', 'current_price'], null]  // But no price data
+                        ['!=', ['string', ['get', 'forsyids']], ''],
+                        ['==', ['get', 'current_price'], null]
                     ]
                 ],
+                '#404040',  // grey color
+                [
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'current_price'],
+                    priceRange.min, priceColors.min,
+                    (priceRange.min + priceRange.max) / 2, priceColors.mid,
+                    priceRange.max, priceColors.max
+                ]
+            ]);
+
+            this.map.setPaintProperty('municipalities-price', 'fill-color', [
+                'case',
+                ['==', ['get', 'current_price'], null],  // Simplified condition
                 '#404040',
                 [
                     'interpolate',
